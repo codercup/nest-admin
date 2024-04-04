01480786d87da020d41c7c0fc7c3ac09cfd43475
菲鸽 <1020103647@qq.com>
2024-04-04 14:07:07 +0800

    chore: 基于 prettier 格式化src目录

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/api');
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('http.port');
-  await app.listen(port);
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('/api')
+  const configService = app.get(ConfigService)
+  const port = configService.get<number>('http.port')
+  await app.listen(port)
 }
-bootstrap();
+bootstrap()
