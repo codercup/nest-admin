1abd5e21d9a3686c164dbf29cf02d5ef6d33ce71
菲鸽 <1020103647@qq.com>
2024-04-04 16:31:32 +0800

    feat: 优化文件返回路径，绝对地址

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
 import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.setGlobalPrefix('/api',{exclude:['']})
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  app.setGlobalPrefix('/api', { exclude: [''] })
+  // 前面一个uplaods表示要进行静态资源托管的目录，后一个uplaods表示用户访问时要带上的前缀
+  app.useStaticAssets('uploads', {
+    // http://localhost:3000/uploads/file-1712218189297-853241156-pretty-girl.png
+    // 如果不带上prefix （或者直接注释掉），则访问路径为 http://localhost:3000/file-1712218189297-853241156-pretty-girl.png
+    prefix:'/uploads'
+  })
   const configService = app.get(ConfigService)
   const port = configService.get<number>('http.port')
   // 这里port改为6000却会有问题，好奇怪
---
+++ src/upload/file-upload-storage.ts
@@ -0,0 +1,19 @@
+import * as multer from "multer";
+import * as fs from 'fs';
+import * as path from "path";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        try {
+            fs.mkdirSync(path.join(process.cwd(), 'uploads'));
+        }catch(e) {}
+
+        cb(null, path.join(process.cwd(), 'uploads'))
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + '-' + file.originalname
+        cb(null, uniqueSuffix)
+    }
+});
+
+export { storage };
