a68986e090649ec2549d6fb3daa5e8eae67006d8
菲鸽 <1020103647@qq.com>
2024-04-04 14:58:56 +0800

    feat: 增加上传模块

---
+++ src/upload/upload.controller.ts
@@ -0,0 +1,22 @@
+import {
+  Post,
+  Get,
+  Controller,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('upload')
+export class UploadController {
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log('file:', file);
+    return file.path;
+  }
+}
