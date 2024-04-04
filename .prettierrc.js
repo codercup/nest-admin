6bcd684c0694ddc8dc00fc4d114c69b7b09d3860
菲鸽 <1020103647@qq.com>
2024-04-04 14:06:52 +0800

    build: 优化 Prettier 配置

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+// https://prettier.io/docs/en/options
+module.exports = {
+  arrowParens: 'avoid', // default "always"
+  trailingComma: 'all', // default "all" "<all|es5|none>"
+  semi: false, // default true
+  singleQuote: true, // default false
+  printWidth: 100, // default 80
+}
