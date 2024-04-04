ebd0e7d5422b6d88ce68b91cb4cb76e4370962dc
菲鸽 <1020103647@qq.com>
2024-04-04 14:04:56 +0800

    feat: 使用yaml配置文件 & 不生成 spec 文件

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  await app.listen(6000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('http.port');
+  await app.listen(port);
 }
 bootstrap();
