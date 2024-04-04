a19cca3b58ef252d8375cf1da873f6e4cd2086f6
菲鸽 <1020103647@qq.com>
2024-04-04 14:27:54 +0800

    feat: 引入user+jwt+mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,57 @@
-import { Module } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import configuration from './config/configuration'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { UsersModule } from './users/users.module'
+import { AuthModule } from './auth/auth.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
+@Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       load: [configuration],
     }),
+    TypeOrmModule.forRootAsync({
+      useFactory(configService: ConfigService) {
+        const mysqlConfig = configService.get<{
+          host: string
+          port: number
+          username: string
+          password: string
+          database: string
+        }>('mysql', {
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: 'root',
+          database: 'nest-study',
+        })
+        console.log('mysqlConfig:', mysqlConfig)
+        return {
+          ...mysqlConfig,
+          type: 'mysql',
+          synchronize: true,
+          logging: false,
+          entities: [],
+          poolSize: 10,
+          connectorPackage: 'mysql2',
+        }
+      },
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: JwtAuthGuard,
+    // },
+  ],
 })
 export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,60 @@
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { ConfigService } from '@nestjs/config'
+
+// This should be a real class/interface representing a user entity
+export type User = any
+
+@Injectable()
+export class UsersService implements OnModuleInit {
+  @Inject()
+  private configService: ConfigService
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'feige',
+      password: '123456',
+    },
+    {
+      userId: 2,
+      username: '菲鸽',
+      password: '888888',
+    },
+  ]
+
+  constructor() {
+    // const dbOptions = this.configService.get('db');
+    // console.log(dbOptions);
+  }
+  onModuleInit() {
+    const dbOptions = this.configService.get('db')
+    console.log('1212', dbOptions)
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user'
+  }
+
+  findAll() {
+    const dbOptions = this.configService.get('db')
+    console.log(dbOptions)
+    return `This action returns all users`
+  }
+
+  findOneFaker(id: number) {
+    return `This action returns a #${id} user`
+  }
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username)
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`
+  }
+}
