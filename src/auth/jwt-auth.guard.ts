c2f06dbed6c738c0ae513450899c9334fe21418b
菲鸽 <1020103647@qq.com>
2024-06-26 21:01:07 +0800

    fix: canActivate 类型错误

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -2,6 +2,8 @@ import { ExecutionContext, Injectable } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { IS_PUBLIC_KEY } from './public.guard'
 import { Reflector } from '@nestjs/core'
+import { Observable } from 'rxjs';
+
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
