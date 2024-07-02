719c2602888823929cfa0035248baa1fec9f5def
菲鸽 <1020103647@qq.com>
2024-07-02 10:48:33 +0800

    feat: 引入prisma并验证通过

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common'
-
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!'
+  }
+  async createUser() {
+    await prisma.user.create({
+      data: {
+        name: '菲鸽',
+        email: 'xx@xx.com',
+      },
+    })
+
+    await prisma.user.create({
+      data: {
+        name: '悟空',
+        email: 'xxx@xxx.com',
+      },
+    })
+
+    const users = await prisma.user.findMany()
+    console.log(users)
   }
 }
